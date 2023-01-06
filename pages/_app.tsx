ad12d5f64faa62b686e43421daf501b6621d89ad
trentteinert <102979377+trentteinert@users.noreply.github.com>
2023-01-05 19:47:00 -0500

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Inconsolata } from '@next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 
 const inconsolata = Inconsolata({
   subsets: ['latin'],
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={inconsolata.className}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
