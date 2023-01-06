d17455f7ec7a4dfcd899da23e38fe780df7786fb
trentteinert <102979377+trentteinert@users.noreply.github.com>
2023-01-05 19:16:33 -0500

    Update SlideShow.tsx

--- components/SlideShow.tsx
+++ components/SlideShow.tsx
@@ -16,12 +16,13 @@ export default function SlideShow({ bio, bioChange, menu, changeMenu }: any) {
   return (
     <>
       <main
+        onClick={navigateSlide}
         className={
           menu
-            ? `select-none relative z-10 transition-all ${
+            ? `h-[100vh] select-none relative z-10 transition-all ${
                 bio ? 'mt-[180px] md:mt-[40px]' : 'mt-[90px] md:mt-[40px]'
               } ml-4 md:ml-52 pb-[60px] pr-4`
-            : 'select-none relative z-10 transition-all mt-10 ml-4 pb-[60px] pr-4'
+            : 'h-[100vh] select-none relative z-10 transition-all mt-10 ml-4 pb-[60px] pr-4'
         }
       >
         <Image
