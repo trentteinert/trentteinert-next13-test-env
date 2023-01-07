2db8f857355086d1a5052922bb3ade7d36ecddc5
trentteinert <102979377+trentteinert@users.noreply.github.com>
2023-01-07 13:26:34 -0500

    Update BioText.tsx

--- components/BioText.tsx
+++ components/BioText.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 export default function BioText() {
   return (
-    <div className={'w-96 mt-1 mb-2 mr-4 md:w-48 text-justify text-sm'}>
+    <div
+      className={'max-w-[500px] mt-1 mb-2 mr-4 md:w-48 text-justify text-sm'}
+    >
       is a artist based in Brooklyn, Nyc. his work explores the results of our
       capitlist society in his local surrounding and how individuals revolt
       against the way they are made to use public space.
