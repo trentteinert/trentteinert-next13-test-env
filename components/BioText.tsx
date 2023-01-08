51e9ff643146d90b020a9fdcb7c49ed6cb7bdb45
trentteinert <102979377+trentteinert@users.noreply.github.com>
2023-01-07 20:37:14 -0500

    Update BioText.tsx

--- components/BioText.tsx
+++ components/BioText.tsx
@@ -6,7 +6,7 @@ export default function BioText() {
       className={'max-w-[500px] mt-1 mb-2 mr-4 md:w-48 text-justify text-sm'}
     >
       is a artist based in Brooklyn, Nyc. his work explores the results of our
-      capitlist society in his local surrounding and how individuals revolt
+      capitalist society in his local surrounding and how individuals revolt
       against the way they are made to use public space.
     </div>
   );
